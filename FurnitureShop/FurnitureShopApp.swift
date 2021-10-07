0df0ab3ced1f1cd7ea44eca84820f2144aeaaa5e
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-07 20:02:44 +0700

    Create the project

---
+++ FurnitureShop/FurnitureShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureShopApp.swift
+//  FurnitureShop
+//
+//  Created by Danh Tu on 07/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
